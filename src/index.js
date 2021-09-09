c3a30125b9533981374ba900a1fd2086945a9c90
Ahmad Zia <ah.ziayosfi@gmail.com>
2021-09-09 12:10:42 +0430

    fix linters issue

--- src/index.js
+++ src/index.js
@@ -1,25 +1,28 @@
-import _ from 'lodash';
-import './style.css'
+import './style.css';
+
 const leaderBoard = [
-    {name:'ahmad',score:100},
-    {name:'ahmad',score:100},
-    {name:'ahmad',score:100},
-    {name:'ahmad',score:100},
-    {name:'ahmad',score:100}];
+  { name: 'ahmad', score: 100 },
+  { name: 'ahmad', score: 100 },
+  { name: 'ahmad', score: 100 },
+  { name: 'ahmad', score: 100 },
+  { name: 'ahmad', score: 100 },
+  { name: 'ahmad', score: 100 },
+  { name: 'ahmad', score: 100 },
+  { name: 'ahmad', score: 100 }];
 const displayList = () => {
   const section = document.getElementById('leaderboard');
   const list = document.createElement('ul');
   list.id = 'list';
   leaderBoard.forEach((row) => {
     const { name, score } = row;
-    let card = `<li class="" >
+    const card = `<li class="" >
     <p>${name}: ${score}</p>
     </li>`;
     list.insertAdjacentHTML('beforeend', card);
   });
   section.innerHTML = '';
   section.appendChild(list);
-    };
+};
 document.addEventListener('DOMContentLoaded', () => {
-    displayList();
-    });
+  displayList();
+});

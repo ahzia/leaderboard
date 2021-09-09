e9ef42bfbae9315671def78476ed1acc9d4b85d4
Ahmad Zia <ah.ziayosfi@gmail.com>
2021-09-10 00:48:58 +0430

    fix linters issue

--- src/index.js
+++ src/index.js
@@ -1,45 +1,46 @@
-import { get } from 'lodash';
 import './css/style.css';
-import Game from './js/Game.js'
+import Game from './js/Game.js';
 
 const displayList = (data) => {
   const section = document.getElementById('leaderboard');
   const list = document.createElement('ul');
   list.id = 'list';
-  if(data!=[]){
-  data.forEach((row) => {
-    const { user, score } = row;
-    const card = `<li class="" >
+  if (data !== []) {
+    data.forEach((row) => {
+      const { user, score } = row;
+      const card = `<li class="" >
     <p>${user}: ${score}</p>
     </li>`;
-    list.insertAdjacentHTML('beforeend', card);
-  });}
+      list.insertAdjacentHTML('beforeend', card);
+    });
+  }
   section.innerHTML = '';
   section.appendChild(list);
 };
 document.addEventListener('DOMContentLoaded', () => {
   const newGame = new Game('New Game');
-  newGame.create().then(()=>{
-    newGame.get().then(() =>{
+  newGame.create().then(() => {
+    newGame.get().then(() => {
       displayList(newGame.data);
-    })
-});
-const submit = document.getElementById('submit');
-const refresh = document.getElementById('refresh');
-submit.addEventListener('click', (event)=>{
-event.preventDefault();
-const name = document.getElementById('name').value;
-const score = document.getElementById('score').value;
-if(name !== '' && score1 !== ''){
-newGame.save(name, score).then(() =>{
-  newGame.get().then(() =>{
-    displayList(newGame.data);
+    });
   });
-});}
-});
-refresh.addEventListener('click' , ()=>{
-  newGame.get().then(() =>{
-    displayList(newGame.data);
+  const submit = document.getElementById('submit');
+  const refresh = document.getElementById('refresh');
+  submit.addEventListener('click', (event) => {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const score = document.getElementById('score').value;
+    if (name !== '' && score !== '') {
+      newGame.save(name, score).then(() => {
+        newGame.get().then(() => {
+          displayList(newGame.data);
+        });
+      });
+    }
   });
-})
+  refresh.addEventListener('click', () => {
+    newGame.get().then(() => {
+      displayList(newGame.data);
+    });
+  });
 });

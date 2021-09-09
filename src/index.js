8d8030f3c7551c00a91a342ebe579c4ca4591be7
Ahmad Zia <ah.ziayosfi@gmail.com>
2021-09-10 00:37:12 +0430

    work on add button functionality

--- src/index.js
+++ src/index.js
@@ -1,28 +1,40 @@
-import './style.css';
+import { get } from 'lodash';
+import './css/style.css';
+import Game from './js/Game.js'
 
-const leaderBoard = [
-  { name: 'ahmad', score: 100 },
-  { name: 'ahmad', score: 100 },
-  { name: 'ahmad', score: 100 },
-  { name: 'ahmad', score: 100 },
-  { name: 'ahmad', score: 100 },
-  { name: 'ahmad', score: 100 },
-  { name: 'ahmad', score: 100 },
-  { name: 'ahmad', score: 100 }];
-const displayList = () => {
+const displayList = (data) => {
   const section = document.getElementById('leaderboard');
   const list = document.createElement('ul');
   list.id = 'list';
-  leaderBoard.forEach((row) => {
-    const { name, score } = row;
+  console.log(data);
+  if(data!=[]){
+  data.forEach((row) => {
+    const { user, score } = row;
     const card = `<li class="" >
-    <p>${name}: ${score}</p>
+    <p>${user}: ${score}</p>
     </li>`;
     list.insertAdjacentHTML('beforeend', card);
-  });
+  });}
   section.innerHTML = '';
   section.appendChild(list);
 };
 document.addEventListener('DOMContentLoaded', () => {
-  displayList();
+  const newGame = new Game('New Game');
+  newGame.create().then(()=>{
+    newGame.get().then(() =>{
+      displayList(newGame.data);
+    })
+});
+const submit = document.getElementById('submit');
+const refresh = document.getElementById('refresh');
+submit.addEventListener('click', (event)=>{
+event.preventDefault();
+const name = document.getElementById('name').value;
+const score = document.getElementById('score').value;
+newGame.save(name, score).then(() =>{
+  newGame.get().then(() =>{
+    displayList(newGame.data);
+  });
+})
+})
 });
